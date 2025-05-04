8d719987fac96295b628204bab63f83b3ccbf846
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-05-04 18:02:06 +0700

    moved to the appropriate folder

---
+++ src/components/fetchImages.js
@@ -0,0 +1,51 @@
+async function getPokemonData() {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=2000`)
+
+        if(!response.ok)
+            throw Error('invalid response bro')
+
+        const rawPokemonData = await response.json() //json from response
+
+        //randomize the pokemon roster
+        let randomizedPokemonsIndexRaw = getRandomNumberArray(12,0,rawPokemonData.results.length)
+        let randomizedPokemonsRaw = []
+
+        for(let i=0; i < randomizedPokemonsIndexRaw.length; i++)
+            randomizedPokemonsRaw.push(rawPokemonData.results[randomizedPokemonsIndexRaw[i]])
+
+        //fetch the pokemon image
+        let pokemonDataResponse = []
+        let pokemonData = []
+        let pokemonImage = []
+
+        for(let i=0; i<randomizedPokemonsRaw.length; i++) {
+            pokemonDataResponse[i] = await fetch(randomizedPokemonsRaw[i].url)
+            pokemonData[i] = await pokemonDataResponse[i].json() //json of pokemon data
+            pokemonImage[i] = {
+                id: crypto.randomUUID(),
+                name: pokemonData[i].name,
+                image: pokemonData[i].sprites.front_default
+            }
+        }
+        console.log(pokemonImage)
+        return pokemonImage
+    }
+    catch(error) {console.error(error)}
+}
+
+getPokemonData()
+
+function getRandomNumberArray(length, min, max) { //randomizer
+    let newArray = [];
+    for (let i = 0; i < length; i++) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      if(!newArray.includes(randomNumber))
+        newArray.push(randomNumber);
+    }
+
+    if(newArray.length < length)
+        newArray = getRandomNumberArray(length, min, max)
+
+    return newArray;
+}
