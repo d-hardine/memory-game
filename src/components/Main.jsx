d9154d161e0bd35c516e6a1590fca86c74c326d4
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-06-03 23:29:52 +0700

    cat memory game done, mobile responsive still need work

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,20 +4,21 @@ import '../styles/Main.css'
 
 export default function Main() {
     const [catImages, setCatImages] = useState([])
+    const [score, setScore] = useState(0)
+    const [bestScore, setBestScore] = useState(0)
+    const [gameOverPopup, setGameOverPopup] = useState(false)
 
     //fetch cat images
-    useEffect(() => {
-        async function fetchImages() {
-            const images = await getCatImages()
-            setCatImages(images)
-        }
+    async function fetchImages() {
+        const images = await getCatImages()
+        setCatImages(images)
+    }
+
+    useEffect(() => {        
         fetchImages()
     }, [])
 
-    function buttonHandler() {
-        console.log(catImages)
-    }
-
+    //clicked cat image handler
     const imageHandler = (e) => {
         let newCatImages = [] //to prevent mutation, react f**king sucks
         for(let i =0; i< catImages.length; i++) {
@@ -26,20 +27,47 @@ export default function Main() {
         let shuffledCatImages = shuffleArray(newCatImages)
         setCatImages(shuffledCatImages)
 
-        console.log(e.target.id)
+        if(e.target.classList.contains('isClicked')) { //if lose
+            e.target.classList.remove('isClicked')
+            fetchImages()
+            setScore(0)
+            setGameOverPopup(true)
+        } else { //keep playing...
+            e.target.classList.add('isClicked')
+            setScore(score + 1)
+            if( score >= bestScore)
+                setBestScore(score + 1)
+        }
     }
 
     return(
         <main>
-            <p>Play Memory Game with Cats!</p>
-            <p>There are 12 different cat images on the board. Click on each card once until you click them all. </p>
-            <br />
-            <button onClick={buttonHandler}>console log catImages</button>
+            <div className="top-container">
+                <div className="left-container">
+                    <p>Play Memory Game with Cats!</p>
+                    <p>There are 12 different cat images on the board. Click on each card once until you click them all. </p>
+                </div>
+                <div className="right-container">
+                    <p>Score: {score}</p>
+                    <p>Best Score: {bestScore}</p>
+                </div>
+            </div>
             <div className="cat-image-container">
                 {catImages.map((catImage) => {
                     return <img className="cat-image" onClick={imageHandler} key={catImage.id} id={catImage.id} src={catImage.url} width={350}/>
                 })}
             </div>
+            {gameOverPopup ?
+                (<div className="overlay">
+                    <div className="popup">
+                        <p><b>GAME OVER</b></p>
+                        <p>Score: {score}</p>
+                        <p>Best Score: {bestScore}</p>
+                        <button onClick={() => setGameOverPopup(false)}>Play Again</button>
+                    </div>
+                </div>)
+                : null
+            }
         </main>
     )
 }
